5bf30d2f2c7113234644b2c43cafec3fc59033fa
duhdanube <Danny.B.Lim@gmail.com>
2020-10-22 18:32:48 +0700

    Created a page template called 'Page 3 Columns' that renders 3 columns via Bootstrap components.

---
+++ page3columns.php
@@ -0,0 +1,20 @@
+<?php /* Template Name: Page 3 Columns */ ?>
+<div class="container">
+	<div class="row">
+		<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+			<h2>Heading</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam sint corrupti, obcaecati vero laboriosam reiciendis, nesciunt, facere impedit dolore amet similique harum officiis corporis. Vero error cum alias quod ad!</p>
+			<button class="btn btn-default">View details >></button>
+		</div>
+		<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+			<h2>Heading</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam sint corrupti, obcaecati vero laboriosam reiciendis, nesciunt, facere impedit dolore amet similique harum officiis corporis. Vero error cum alias quod ad!</p>
+			<button class="btn btn-default">View details >></button>
+		</div>
+		<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+			<h2>Heading</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam sint corrupti, obcaecati vero laboriosam reiciendis, nesciunt, facere impedit dolore amet similique harum officiis corporis. Vero error cum alias quod ad!</p>
+			<button class="btn btn-default">View details >></button>
+		</div>
+	</div>
+</div>
